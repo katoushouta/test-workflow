8faf49b7bdebbd3bb5562598d2915a0dd65778e9
katoushouta <shouta.katou@link-u.co.jp>
2023-12-04 14:17:02 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    document.getElementById('startWork').addEventListener('click', () => {
+        console.log('出勤しました');
+    });
+
+    document.getElementById('endWork').addEventListener('click', () => {
+        console.log('退勤しました');
+    });
+
+    document.getElementById('startBreak').addEventListener('click', () => {
+        console.log('休憩を開始しました');
+    });
+
+    document.getElementById('endBreak').addEventListener('click', () => {
+        console.log('休憩から戻りました');
+    });
+});
